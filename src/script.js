eb0d6a1d2f2517b61d727f223983244bfd0a0920
KCasertz <77019668+KCasertz@users.noreply.github.com>
2021-03-20 15:44:00 +0000

    Corrected descriptions
    Added if statements to ensure the API descriptions were grammatically correct

--- src/script.js
+++ src/script.js
@@ -45,6 +45,19 @@ function getTemp(response) {
   cityName.innerHTML = response.data.name;
   let weatherDescriptionText = document.querySelector("#weather-description-text")
   weatherDescriptionText.innerHTML = response.data.weather[0].main;
+  if (response.data.weather[0].main === "Thunderstorm") {
+    weatherDescriptionText.innerHTML = "thunderstorms"
+  }
+  if (response.data.weather[0].main === "Clear") {
+    weatherDescriptionText.innerHTML = "it's pretty clear"
+  }
+  if (response.data.weather[0].main === "Clouds") {
+    weatherDescriptionText.innerHTML = "it's pretty cloudy"
+  }
+  if (response.data.weather[0].main === "Tornado") {
+    weatherDescriptionText.innerHTML = "a tornado's coming"
+  }
+  
 }
 
 function askTempMetric() {
